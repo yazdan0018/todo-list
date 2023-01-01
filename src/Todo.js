d7efea2de8c8481170984be545c6d788c7a64465
yazdan <apple.yazdan@ymailcom>
2023-01-01 22:10:49 +0330

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ACTIONS} from './App';
+
+function Todo({todo,dispatch}){
+  return(
+    <div>
+      <span style={{color : todo.completed ? '#aaa' : '#000'}}>{todo.name}</span>
+      <button onClick={() => dispatch({type : ACTIONS.TOGGLE,payload : {id : todo.id}})}>Toggle</button>
+      <button onClick={() => dispatch({type : ACTIONS.DELETE ,payload : {id : todo.id}})}>Delete</button>
+    </div>
+  )
+}
+
+export default Todo;
