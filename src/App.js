c47b1df2f6d334bc3d494b2230739400f89851ed
yazdan <apple.yazdan@ymailcom>
2023-01-06 14:52:22 +0330

    add styled components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { useState } from "react";
 import Todo from "./Todo";
 import useTodos from "./hooks/useTodos";
 
+// add validation for form
+// add styled components
+// add button for form
+// show no todos found error
+// add modal for form
+
 function App () {
   const {addTodo, removeTodo, todos, toggleTodo} = useTodos();
   const [name, setName] = useState('');
